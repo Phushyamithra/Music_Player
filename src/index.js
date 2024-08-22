5f9adbe5c4662ff2f1ee6251a86b6d3e8fa99c87
Phushyamithra <gauriphushyamithra@gmail.com>
2024-08-22 22:31:57 +0530

    Responsive commit done

--- src/index.js
+++ src/index.js
@@ -2,14 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { StateProvider } from './utils/StateProvider';
-import reducer, { initialState } from './utils/reducer.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <StateProvider initialState={initialState} reducer={reducer}>
-      <App />
-    </StateProvider>
+    <App />
   </React.StrictMode>
 );
